bdabcb261782f54d4502cbdb2bf60418e509abe0
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-02-09 21:33:22 +0530

    Weather App Development Is Inprogress

--- miniwork/skyCast/script.js
+++ miniwork/skyCast/script.js
@@ -1,4 +1,30 @@
-const url = 'https://open-weather13.p.rapidapi.com/city/New%20York'
+/**
+ * Weather App
+ * TODO: Complete getWeatherData() to return json response Promise
+ * TODO: Complete searchCity() to get user input and get data using getWeatherData()
+ * TODO: Complete showWeatherData() to set the data in the the html file from response
+ */
+
+/* DIV ID's you'll need access to 👇
+"city-name"
+"weather-type"
+"temp"
+"min-temp"
+"max-temp"
+*/
+
+// API_KEY for maps api
+let API_KEY = "39507d026amsh728e93906c1c555p1b839cjsnf8f650992b1e";
+
+/**
+ * Retrieve weather data from openweathermap
+ * HINT: Use fetch()
+ * HINT: URL should look like this: 
+ * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
+ */
+
+
+const URL = "https://open-weather13.p.rapidapi.com/city/";
 const options = {
     method: 'GET',
     headers: {
@@ -7,15 +33,37 @@ const options = {
     }
 }
 
-const getWeatherInfo = async () => {
 
+const getWeatherData = async (city) => {
     try {
-        const response = await fetch(url, options)
+        const response = await fetch(URL+city, options)
         const result = await response.json()
         console.log(result)
+        if(result.cod==200){
+            showWeatherData(result)
+        }
     } catch (error) {
         console.log(error)
     }
+
 }
 
-getWeatherInfo()
+/**
+ * Retrieve city input and get the weather data
+ * HINT: Use the promise returned from getWeatherData()
+ */
+const searchCity = () => {
+    const city = document.getElementById('city').value;
+    getWeatherData(city)
+}
+
+/**
+ * Show the weather data in HTML
+ * HINT: make sure to console log the weatherData to see how the data looks like
+ */
+const showWeatherData = (weatherData) => {
+    console.log(weatherData.name)
+    console.log(weatherData.main.temp)
+    console.log(weatherData.main.temp_min)
+    console.log(weatherData.main.temp_max)
+}
