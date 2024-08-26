2f759cb96f9d7ac134a4742d6a2a0d6191e36fe6
NJagatheshwaran <testjaga001@gmail.com>
2024-08-26 21:32:13 +0530

    JS Revise Basics - Variables and Constant

---
+++ js-basics/index.js
@@ -0,0 +1,31 @@
+ // This is the first code of JavaScript Revise Series!
+ console.log('Hello World!')
+
+ // Variables
+ 
+ // 1. Can't be a reserved word
+ // 2. Must be meaningful name
+ // 3. Can't start with a number
+ // 4. Can't start with a space or hypen(-)
+ // 5. They are case sensitive
+
+ // Before ES6, the keyword used for declaring the variable is var. But after ES6, the recommended
+ // approach is using the let keyword.
+
+ let userName = 'Jaga'
+ console.log(userName);
+
+ // Multiple variable declaration - Old style
+ let firstName, lastName;
+
+ // Multiple variable declaration - New style (Recommended)
+ let userFirstName = 'John';
+ let userLastName = 'Smith';
+
+ // Constant
+
+ // If you don't want to re-initialize / modify the value. Then, declare the variable as Const.
+
+ const interestRate = 0.5;
+ interestRate = 1.0;
+ console.log(age);
