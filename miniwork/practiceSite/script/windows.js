cfc54c251d0ab59cd4165664106874fd65537499
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2025-02-01 19:25:11 +0530

    Windows Page Implementation Completed.

---
+++ miniwork/practiceSite/script/windows.js
@@ -0,0 +1,26 @@
+function openWindowAsTab() {
+  const newTab = window.open("about:blank", "_blank");
+  newTab.document.write(
+    "<h1 style='font-family: Poppins, sans-serif; color: #444;'>You have opened a new window as a Tab</h1>"
+  );
+  newTab.document.close();
+  updateResult("You have opened a new window as a Tab.");
+}
+
+function openWindowAsWindow() {
+  const newWindow = window.open(
+    "about:blank",
+    "_blank",
+    "width=600,height=400"
+  );
+  newWindow.document.write(
+    "<h1 style='font-family: Poppins, sans-serif; color: #444;'>You have opened a new window</h1>"
+  );
+  newWindow.document.close();
+  updateResult("You have opened a new window.");
+}
+
+function updateResult(message) {
+  let resultSection = document.getElementById("result");
+  resultSection.textContent = message;
+}
