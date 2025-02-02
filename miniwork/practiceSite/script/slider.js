a9861efb337b1fd64b3fd38524e00a2cbd506c5f
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2025-02-02 19:58:39 +0530

    Slider Page Implementation Completed.

---
+++ miniwork/practiceSite/script/slider.js
@@ -0,0 +1,8 @@
+function updateResult() {
+  let slider = document.getElementById("slider");
+  let sliderValue = slider.value;
+  // document.getElementById('sliderValue').innerText = sliderValue;
+  document.getElementById(
+    "result"
+  ).textContent = `You have selected value: ${sliderValue}`;
+}
