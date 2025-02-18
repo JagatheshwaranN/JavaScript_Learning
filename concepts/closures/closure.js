6d42e4cc46983a511158d79afd673cec2871aeab
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2025-02-18 09:26:58 +0530

    Closures Review Completed

--- concepts/closures/closure.js
+++ concepts/closures/closure.js
@@ -4,12 +4,22 @@
 // which that function was declared. This environment consists of any variables
 // that were in-scope at the time the closure was created.
 function makeFunc() {
-  const name = "Jaga";
-  function displayName() {
-    console.log(name);
+  let name1 = "Jaga";
+  function displayName(newName) {
+    name1 = newName;
+    console.log(name1);
   }
   return displayName;
 }
-
+// name1 = "Johny";
 const myfunc = makeFunc();
-myfunc();
+// name = "Johny";
+myfunc("Johny");
+myfunc("Jackson");
+
+// The closure captures the reference to name inside the function’s lexical 
+// environment at the time makeFunc() was called.
+
+// Even though you reassign name = "Another Name";, that’s a completely different
+// variable in the global scope, which doesn’t affect the closure’s reference inside
+// makeFunc().
